29336ecce9bce0aeb3aa7c73a14d8bf8ead6ea1c
Loxstomper <Loxstomper@users.noreply.github.com>
2016-03-28 20:31:12 +1000

    Added server
    Server that receives data from the pi in the bin, adds some more values and stores it into a sqlite3 database.

---
+++ server.py
@@ -0,0 +1,66 @@
+import sqlite3
+import socket
+import time
+
+# server
+host = ''
+port = 5555  # ask IT at school about what ports I can use
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # ipv4 / tcp connection
+
+# database
+db = sqlite3.connect('rubbish.db')
+db_c = db.cursor()
+
+
+def create_table():
+    db_c.execute('CREATE TABLE IF NOT EXISTS rubbish(date TEXT, day TEXT, time TEXT, sound TEXT)')
+
+
+def get_get_data_add_to_db():
+    conn, addr = s.accept()
+    data = conn.recv(2048)  # buffer of 2048 bytes
+    print('Received data from: ', addr)
+    data = data.decode()
+    rubbish_time, sound = data.split()
+
+    # server side creations no point wasting cpu cycles on the one in the bin
+    date = str(time.strftime('%d-%m-%Y'))
+    day = str(time.strftime('%A'))
+
+    # have to reconnect to database because i close connection every iteration
+    db = sqlite3.connect('rubbish.db')
+    db_c = db.cursor()
+
+    # add to db and save
+    db_c.execute("INSERT INTO rubbish(date, day, time, sound) VALUES (?, ?, ?, ?)",
+                 (date, day, rubbish_time, sound))
+    print("Added values to database: ", date, ",", day, ",", rubbish_time, ",", sound)
+    db.commit()
+    print('Saved changes to database.')
+
+    # close connection to db
+    db_c.close()
+    db.close()
+    print('Closed the database.')
+    print('Waiting for a connection...\n')
+
+# Program
+
+# set up server
+try:
+    s.bind((host,port))
+except socket.error as e:
+    print(str(e))
+
+s.listen(5) # max of 5 connections simultaneously
+print("waiting for a connection")
+
+# create the table if it doesnt exist
+create_table()
+
+while True:
+    get_get_data_add_to_db()
+
+
+
+
